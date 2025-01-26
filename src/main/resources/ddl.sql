45a47138ee1e9b61b77959cac2776c1d77019e2d
mkbajer <maciej.bajer@interia.pl>
2025-01-26 19:37:27 +0100

    Added dml file to create data in db, refactored model,ddl,database

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -7,19 +7,42 @@ create table if not exists market_places (
     PRIMARY KEY (id)
 );
 
-create table if not exists users (
+create table if not exists user (
     id serial,
     market_places_id BIGINT UNSIGNED NOT NULL,
     name VARCHAR(45) NOT NULL,
     surname VARCHAR(45) NOT NULL,
     email VARCHAR(45) NOT NULL,
     password VARCHAR(15) NOT NULL,
-    phone VARCHAR(45),
+    phone INTEGER,
     type tinyint(0) NOT NULL DEFAULT 0,
     active tinyint(0) NOT NULL DEFAULT 0,
-    market_places_id BIGINT,
     PRIMARY KEY (id),
     CONSTRAINT fk_users_market_places_id FOREIGN KEY(market_places_id) REFERENCES market_places(id)
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
+create table if not exists cart (
+    id serial,
+    PRIMARY KEY (id)
+);
+
+create table if not exists orders (
+    id serial,
+    cart_id BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT orders_cart_id FOREIGN KEY (cart_id) REFERENCES cart(id)
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
+create table if not exists shipment (
+    id serial,
+    orders_id BIGINT UNSIGNED NOT NULL,
+    courier VARCHAR(45),
+    PRIMARY KEY (id),
+    CONSTRAINT shipment_orders_id FOREIGN KEY (orders_id) REFERENCES orders(id)
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 );
@@ -38,36 +61,12 @@ create table if not exists address (
         ON DELETE NO ACTION
 );
 
-create table if not exists shipment (
-    id serial,
-    order_id BIGINT UNSIGNED NOT NULL,
-    courier VARCHAR(45),
-    PRIMARY KEY (id),
-    CONSTRAINT shipment_order_id FOREIGN KEY (order_id) REFERENCES orders(id)
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-);
-
-create table if not exists orders (
-    id serial,
-    cart_id BIGINT UNSIGNED NOT NULL,
-    PRIMARY KEY (id),
-    CONSTRAINT orders_cart_id FOREIGN KEY (cart_id) REFERENCES cart(id)
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-);
-
-create table if not exists cart (
-    id serial,
-    PRIMARY KEY (id)
-);
-
 create table if not exists payment (
     id serial,
-    order_id BIGINT UNSIGNED NOT NULL,
+    orders_id BIGINT UNSIGNED NOT NULL,
     type VARCHAR(45),
     PRIMARY KEY (id),
-    CONSTRAINT payment_order_id FOREIGN KEY (order_id) REFERENCES orders(id)
+    CONSTRAINT payment_orders_id FOREIGN KEY (orders_id) REFERENCES orders(id)
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 );
@@ -91,7 +90,7 @@ create table if not exists discount (
 
 create table if not exists product (
     id serial,
-    category_id UNSIGNED NOT NULL,
+    category_id BIGINT UNSIGNED NOT NULL,
     name VARCHAR(45),
     price DOUBLE,
     PRIMARY KEY (id),
